5bd5b546bd49bc019077900bfdec469340c83f3d
Igoris Ivanovas <igoris.x@inbox.lt>
2024-06-06 17:40:39 +0200

    progres with octokit instal ans setup

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
+import "./App.sass";
+import { Octokit } from "octokit";
 
-import './App.sass';
+const octokit = new Octokit({
+  auth: process.env.TOKEN,
+});
 
-function App() {
-  return (
-    <div className="App">
-     here will be content 
-    </div>
+try {
+  const result = await octokit.request("GET /repos/{owner}/{repo}/issues", {
+    owner: "octocat",
+    repo: "Spoon-Knife",
+  });
+
+  result.data.map((issue) => {
+    console.log(issue.title, issue.user.id);
+  });
+  // const titleAndAuthor = result.data.map(issue => {title: issue.title, authorID: issue.user.id})
+
+  // console.log(titleAndAuthor)
+} catch (error) {
+  console.log(
+    `Error! Status: ${error.status}. Message: ${error.response.data.message}`
   );
+}
+function App() {
+  return <div className="App">here will be content</div>;
 }
 
 export default App;
