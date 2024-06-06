5bb8db3234d8474fcc0bd04c2ef21e7169b467fe
Igoris Ivanovas <igoris.x@inbox.lt>
2024-06-06 17:48:17 +0200

    in progress;)

--- src/App.js
+++ src/App.js
@@ -6,17 +6,23 @@ const octokit = new Octokit({
 });
 
 try {
-  const result = await octokit.request("GET /repos/{owner}/{repo}/issues", {
-    owner: "octocat",
-    repo: "Spoon-Knife",
+  const result = await octokit.request('GET /search/igoris12', {
+  // owner: 'igoris12',
+  // repo: 'portfolio_website',
+  headers: {
+    'X-GitHub-Api-Version': '2022-11-28'
+  }
   });
 
-  result.data.map((issue) => {
-    console.log(issue.title, issue.user.id);
-  });
-  // const titleAndAuthor = result.data.map(issue => {title: issue.title, authorID: issue.user.id})
+  // result.data.map((issue) => {
+  //   console.log(issue.title, issue.user.id);
+  // });
 
-  // console.log(titleAndAuthor)
+  
+    console.log(result.data);
+
+  
+  
 } catch (error) {
   console.log(
     `Error! Status: ${error.status}. Message: ${error.response.data.message}`
