40487ae8c02d86d1acb10c103baec6d0ae69054a
Igoris Ivanovas <igoris.x@inbox.lt>
2024-06-10 18:10:55 +0200

    udate got it  now aaaaa

--- src/App.js
+++ src/App.js
@@ -2,27 +2,22 @@ import "./App.sass";
 import { Octokit } from "octokit";
 
 const octokit = new Octokit({
-  auth: secret ,
+  auth: process.env.TOKEN
 });
 
 try {
-  const result = await octokit.request('GET /repos/{owner}/{repo}', {
-  owner: 'igoris12',
-  repo: 'portfolio_website',
-  headers: {
-    'X-GitHub-Api-Version': '2022-11-28'
-  }
+  const result = await octokit.request("GET /repos/{owner}/{repo}", {
+    owner: "igoris12",
+    repo: "portfolio_website",
+    headers: {
+      "X-GitHub-Api-Version": "2022-11-28",
+    },
   });
 
-  // result.data.map((issue) => {
-  //   console.log(issue.title, issue.user.id);
-  // });
-
-  
-    console.log(result.data);
-
-  
-  
+ 
+  console.log(result.data);
+  console.log(result.data.name);
+  console.log(result.data.svn_url);
 } catch (error) {
   console.log(
     `Error! Status: ${error.status}. Message: ${error.response.data.message}`
