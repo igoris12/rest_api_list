d29b9659fd18adbe955689fa26a3d6ff4109dbb6
Igoris Ivanovas <igoris.x@inbox.lt>
2024-06-20 19:25:49 +0200

    in pregress

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import "./App.sass";
 import React, { useEffect, useState } from "react";
 
 const App = () => {
-  const url = 'https://api.github.com/search/repositories?q=readme:pomodoro=stars&order=desc&per_page=10';
+  const url = 'https://api.github.com/search/repositories?q=Pomodoro&type=repositories&s=stars&o=desc';
 
   fetch(url)
     .then(response => response.json())
     .then(repos => {
       console.log(repos);
+      repos.items.map(repo => console.log(repo.name))
     });
   return <div className="App">here will be content</div>;
 }
