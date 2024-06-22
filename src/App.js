205cca0655a55eb9cbeb855fc2a4195fa48382bf
Igoris Ivanovas <igoris.x@inbox.lt>
2024-06-22 18:50:38 +0200

    make some pregress

--- src/App.js
+++ src/App.js
@@ -2,15 +2,37 @@ import "./App.sass";
 import React, { useEffect, useState } from "react";
 
 const App = () => {
-  const url = 'https://api.github.com/search/repositories?q=Pomodoro&type=repositories&s=stars&o=desc';
+  const [data, setData] = useState([]);
+  const url =
+    "https://api.github.com/search/repositories?q=stars:>=1&sort=stars&order=desc";
 
-  fetch(url)
-    .then(response => response.json())
-    .then(repos => {
-      console.log(repos);
-      repos.items.map(repo => console.log(repo.name))
-    });
-  return <div className="App">here will be content</div>;
-}
+  useEffect(() => {
+    fetch(url)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        setData([...data.items.slice(0, 10)]);
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  }, []);
+  console.log(data);
+  const nameList = data.map((project) => (
+    <li key={project.id}>
+      {project.name} {" "} <span>{project.stargazers_count}</span>
+    </li>
+  ));
+  return (
+    <div className="App">
+      <h1>Content</h1>
+      <ul>{nameList}</ul>
+    </div>
+  );
+};
 
 export default App;
