e10734c37ef4d94ea6136a5308d325b9a3a7252b
Igoris Ivanovas <igoris.x@inbox.lt>
2024-07-01 18:42:55 +0200

    get start num

--- src/components/RepositoriesListItem.jsx
+++ src/components/RepositoriesListItem.jsx
@@ -3,7 +3,11 @@ import React from "react";
 function RepositoriesListItem({ repoData, rank }) {
   console.log(repoData);
   return repoData !== undefined ? (
-    <li>{`Rank: ${rank} Title: ${repoData.name}  Year: ${repoData.created_at.slice(0,4)} Author: ${repoData.owner.login}`}</li>
+    <li>{`Rank: ${rank} Title: ${
+      repoData.name
+    }  Year: ${repoData.created_at.slice(0, 4)} Author: ${
+      repoData.owner.login
+    } Stars: ${repoData.stargazers_count}`}</li>
   ) : (
     <li>lodding...</li>
   );
