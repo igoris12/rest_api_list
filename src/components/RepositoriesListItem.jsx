6edbcc520d88bde8980e68f90ea49a90258c20cb
Igoris Ivanovas <igoris.x@inbox.lt>
2024-07-01 18:38:26 +0200

    get creator login

--- src/components/RepositoriesListItem.jsx
+++ src/components/RepositoriesListItem.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function RepositoriesListItem({ repoData, rank }) {
   console.log(repoData);
   return repoData !== undefined ? (
-    <li>{rank + " " + repoData.name + " Year: " + repoData.created_at.slice(0,4)}</li>
+    <li>{`Rank: ${rank} Title: ${repoData.name}  Year: ${repoData.created_at.slice(0,4)} Author: ${repoData.owner.login}`}</li>
   ) : (
     <li>lodding...</li>
   );
