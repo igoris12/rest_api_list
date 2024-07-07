2fd08c4217a515818bd59385285857a11d0f806b
Igoris Ivanovas <igoris.x@inbox.lt>
2024-07-07 16:10:30 +0200

    add info element

--- src/components/RepositoriesListItem.jsx
+++ src/components/RepositoriesListItem.jsx
@@ -3,13 +3,22 @@ import { FaRegStar } from "react-icons/fa";
 
 function RepositoriesListItem({ repoData, rank }) {
   return repoData !== undefined ? (
-    <li className="RepositoriesListItem">
-      <span className="rank">{rank}</span> 
-      <span className="title">{repoData.name}</span>
-      <span className="name">{repoData.owner.login}</span>
-      <span className="year">{repoData.created_at.slice(0, 4)}</span>
-      <span className="stars"><FaRegStar />{ repoData.stargazers_count}</span>
-    </li>
+    <>
+      <li className="RepositoriesListItem">
+        <span className="rank">{rank}</span>
+        <span className="title">{repoData.name}</span>
+        <span className="name">{repoData.owner.login}</span>
+        <span className="year">{repoData.created_at.slice(0, 4)}</span>
+        <span className="stars">
+          <FaRegStar />
+          {repoData.stargazers_count}
+        </span>
+      </li>
+      <div className="RepositoriesListItem info">
+        <span className="name">{repoData.owner.login}</span>
+        <span className="year">{repoData.created_at.slice(0, 4)}</span>
+      </div>
+    </>
   ) : (
     <li>lodding...</li>
   );
