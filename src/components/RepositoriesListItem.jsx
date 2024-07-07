673d80a064258f8a2ccb1555d52b2c216bead398
Igoris Ivanovas <igoris.x@inbox.lt>
2024-07-07 16:32:42 +0200

    make info responcive layout

--- src/components/RepositoriesListItem.jsx
+++ src/components/RepositoriesListItem.jsx
@@ -3,22 +3,20 @@ import { FaRegStar } from "react-icons/fa";
 
 function RepositoriesListItem({ repoData, rank }) {
   return repoData !== undefined ? (
-    <>
-      <li className="RepositoriesListItem">
-        <span className="rank">{rank}</span>
-        <span className="title">{repoData.name}</span>
-        <span className="name">{repoData.owner.login}</span>
-        <span className="year">{repoData.created_at.slice(0, 4)}</span>
-        <span className="stars">
-          <FaRegStar />
-          {repoData.stargazers_count}
-        </span>
-      </li>
-      <div className="RepositoriesListItem info">
-        <span className="name">{repoData.owner.login}</span>
-        <span className="year">{repoData.created_at.slice(0, 4)}</span>
+    <li className="RepositoriesListItem">
+      <span className="rank">{rank}</span>
+      <span className="title">{repoData.name}</span>
+      <span className="name">{repoData.owner.login}</span>
+      <span className="year">{repoData.created_at.slice(0, 4)}</span>
+      <span className="stars">
+        <FaRegStar />
+        {repoData.stargazers_count}
+      </span>
+      <div className="info">
+        <span className="name">Author: {repoData.owner.login}</span>
+        <span className="year">Year: {repoData.created_at.slice(0, 4)}</span>
       </div>
-    </>
+    </li>
   ) : (
     <li>lodding...</li>
   );
